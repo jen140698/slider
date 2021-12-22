777d96381b067d6733f5b9873875789eb13b4be9
jenvihadiyal <jenvihadiyal@gmail.com>
2021-12-22 22:05:47 +0530

    remove unused code

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,30 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-const responsives = {
-    superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 4
-    },
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 4
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 750 },
-        items: 3,
-    },
-    miniscreen: {
-        breakpoint: { max: 750, min: 464 },
-        items: 2
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-        classNames: "testing"
-    }
-};
 
 const responsive = {
     superLargeDesktop: {
